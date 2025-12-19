01f2c81b40f64e47818274b7b6560c73c4cddd7d
bala-amypo <bala@amypo.in>
2025-12-19 06:27:55 +0000

    User commit

--- src/main/java/com/example/demo/entity/StudentEntity.java
+++ src/main/java/com/example/demo/entity/StudentEntity.java
@@ -18,49 +18,49 @@ public class StudentEntity{
      private String password;
      private Date created;
 
-public Integer getId(){
-    return id;
-}
-public void setId(Integer id ){
-    this.id=id;
-}
+// public Integer getId(){
+//     return id;
+// }
+// public void setId(Integer id ){
+//     this.id=id;
+// }
  
- public String getUsername(){
-   return username;
- }
- public void setUsername(String username){
-    this.username=username;
- }
- public String getEmail(){
-    return email;
- }
- public void setEmail(String email){
-    this.email=email;
- }
- public String getPassword(){
-    return password;
- }
- public void setPassword(String password){
-    this.password=password;
- }
+//  public String getUsername(){
+//    return username;
+//  }
+//  public void setUsername(String username){
+//     this.username=username;
+//  }
+//  public String getEmail(){
+//     return email;
+//  }
+//  public void setEmail(String email){
+//     this.email=email;
+//  }
+//  public String getPassword(){
+//     return password;
+//  }
+//  public void setPassword(String password){
+//     this.password=password;
+//  }
 
- public Date getCreated(){
-    return created;
- }
- public void setCreated(Date created){
-    this.created=created;
- }
- public StudentEntity(Integer id,String username,String email,String password,Date created){
-    this.id=id;
-    this.username=username;
-    this.email=email;
-    this.password=password;
-    this.created=created;
+//  public Date getCreated(){
+//     return created;
+//  }
+//  public void setCreated(Date created){
+//     this.created=created;
+//  }
+//  public StudentEntity(Integer id,String username,String email,String password,Date created){
+//     this.id=id;
+//     this.username=username;
+//     this.email=email;
+//     this.password=password;
+//     this.created=created;
+
+//  }
+//  public StudentEntity(){
+
+//  }
+
 
  }
- public StudentEntity(){
-
- }
-
-
-}
