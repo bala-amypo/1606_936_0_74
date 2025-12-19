7e79904fc1bef2515db0744cd463dc1a9e300231
bala-amypo <bala@amypo.in>
2025-12-19 10:18:54 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/IdCardService.java
@@ -0,0 +1,11 @@
+package com.example.demo.service;
+import com.example.demo.entity.IdCard;
+import java.util.List;
+
+
+public interface IdCardService{
+
+   StudentDetails Entity postdata(StudentEntity studet);
+
+
+}
---
+++ src/main/java/com/example/demo/service/StudentDetailService.java
@@ -0,0 +1,11 @@
+package com.example.demo.service;
+import com.example.demo.entity.StudentDetailsEntity;
+import java.util.List;
+
+
+public interface StudentDetailsService{
+
+   StudentDetails Entity postdata(StudentEntity studet);
+
+
+}
