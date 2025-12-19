949e4a54bb501f1bc09d8106cc426bc7a79ee3a9
bala-amypo <bala@amypo.in>
2025-12-19 08:32:30 +0000

    User commit

--- src/main/java/com/example/demo/entity/TimeStampEntity.java
+++ src/main/java/com/example/demo/entity/TimeStampEntity.java
@@ -1,6 +1,9 @@
 package com.example.demo.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.GeneratedValue;
 import java.time.LocalDateTime;
 import lombok.Data;
 import lombok.NoArgsConstructor;
