f29a7d4c50d62daf20a925730d710379ac059141
bala-amypo <bala@amypo.in>
2025-12-19 10:39:06 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/StudentDetailsImpl.java
+++ src/main/java/com/example/demo/service/Impl/StudentDetailsImpl.java
@@ -3,20 +3,20 @@ import org.springframework.stereotype.Service;
 import com.example.demo.service.StudentDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.example.demo.repository.StudentDetailsRepository;
-import com.example.demo.entity.StudentdetailsEntity;
+import com.example.demo.entity.StudentDetailsEntity;
 import java.util.List;
 //import org.springframework.web.bind.annotation.PathVariable;
 @Service
-public class StudentD implements IdCardService{
-            @Autowired IdCardRepository idr;
+public class StudentDetailsImpl implements StudentDetailsService{
+            @Autowired StudentDetailsRepository sdi;
             //save()-----these are all inbuilt
             //findall()
             //findById()
             //deleteById()
             //existById()
 @Override
-    public IdCard postdata(IdCard idc){
-         return idr.save(idc);
+    public StudentDetailsEntity postdata(StudentDetailsEntity sde){
+         return sdi.save(sde);
          
     }
    
