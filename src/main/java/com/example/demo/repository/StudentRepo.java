e67bff47d62dd30225c44a97341d07c9e94ae320
bala-amypo <bala@amypo.in>
2025-12-24 06:10:59 +0000

    User commit

--- src/main/java/com/example/demo/repository/StudentRepo.java
+++ src/main/java/com/example/demo/repository/StudentRepo.java
@@ -8,5 +8,5 @@ import org.springframework.stereotype.Repository;
 @Repository 
 public interface StudentRepo extends JpaRepository<StudentsStructure,Integer>{
     @Query("SELECT * FROM StudentsStructure s WHERE s.stuEmail=:email")
-            
+    List<StudentsStructure>findEmail    
 }                              
